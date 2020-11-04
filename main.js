f94798b5b5a5794dae92a13f2fea5486b9fc2b36
Devin Stigter <57394825+Hengselman@users.noreply.github.com>
2020-11-04 12:27:37 +0100

    webp en sizes in jpg toegevoegd

--- main.js
+++ main.js
@@ -14,13 +14,47 @@ hart4 = document.getElementById('hartje4');
 hart5 = document.getElementById('hartje5');
 
 function show1() {
-  this.style.zindex = "1";
+  console.log("Hallooo");
+  hart1.style.zIndex = "1";
+}
+
+function show2() {
+  console.log("Hallooo");
+  hart2.style.zIndex = "1";
+}
+
+function show3() {
+  console.log("Hallooo");
+  hart3.style.zIndex = "1";
+}
+
+function show4() {
+  console.log("Hallooo");
+  hart4.style.zIndex = "1";
+}
+
+function show5() {
+  console.log("Hallooo");
+  hart5.style.zIndex = "1";
 }
 
 function reset() {
-  this.style.zindex = "-1";
+  document.getElementById('hartje1').style.zIndex = "-1";
+  document.getElementById('hartje2').style.zIndex = "-1";
+  document.getElementById('hartje3').style.zIndex = "-1";
+  document.getElementById('hartje4').style.zIndex = "-1";
+  document.getElementById('hartje5').style.zIndex = "-1";
 }
 
 
-hart1.addEventListener("mouseover",show1);
-hart1.addEventListener("mouseout",reset);
+figure1.addEventListener("mouseover",show1);
+figure2.addEventListener("mouseover",show2);
+figure3.addEventListener("mouseover",show3);
+figure4.addEventListener("mouseover",show4);
+figure5.addEventListener("mouseover",show5);
+
+figure1.addEventListener("mouseout",reset);
+figure2.addEventListener("mouseout",reset);
+figure3.addEventListener("mouseout",reset);
+figure4.addEventListener("mouseout",reset);
+figure5.addEventListener("mouseout",reset);
